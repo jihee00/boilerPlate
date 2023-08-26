6b96d36daf2cefa3dc7452fc761d64e8a6275045
Jihee Kim <warmgrey23@naver.com>
2023-08-25 23:37:41 -0400

    user info save

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://jj:abcd1234@cluster0.35srof1.mongodb.net/?retryWrites=true&w=majority', {
@@ -9,6 +17,18 @@ mongoose.connect('mongodb+srv://jj:abcd1234@cluster0.35srof1.mongodb.net/?retryW
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+  })
+  })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
