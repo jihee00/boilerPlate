2522356582db081032e31dc8f294ef61ce85fa20
Jihee Kim <warmgrey23@naver.com>
2023-08-26 23:19:39 -0400

    change port - nodemon test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 4999
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 
